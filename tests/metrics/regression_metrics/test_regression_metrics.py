b6911fc6161e3eeaa623b7b716e95111d8fb5626
Yu-Lin Huang <yu-lin.huang@uni.lu>
2025-11-21 15:59:12 +0100

    Ruff format

--- tests/metrics/regression_metrics/test_regression_metrics.py
+++ tests/metrics/regression_metrics/test_regression_metrics.py
@@ -1,15 +1,19 @@
-import datetime
 import numpy as np
 import pandas as pd
 import pytest
 import uuid
-import onnxruntime as ort
 
-from a4s_eval.data_model.evaluation import Dataset, DataShape, Feature, FeatureType, Model
+from a4s_eval.data_model.evaluation import (
+    Dataset,
+    DataShape,
+    Feature,
+    FeatureType,
+    Model,
+)
 from a4s_eval.metrics.regression_metrics.perf_metrics import (
     empty_regression_metric,
     simple_demo_metric,
-    mean_squared_error_metric
+    mean_squared_error_metric,
 )
 
 
@@ -22,7 +26,7 @@ def data_shape() -> DataShape:
         min_value=0,
         max_value=0,
     )
-    
+
     target = Feature(
         pid=uuid.uuid4(),
         name="target",
@@ -38,23 +42,31 @@ def data_shape() -> DataShape:
 
 @pytest.fixture(scope="module")
 def test_dataset(data_shape: DataShape) -> Dataset:
-    data = pd.DataFrame({
-        "date": ["2021-11-23 00:00:00", "2021-11-24 00:00:00", "2021-11-25 00:00:00"],
-        "feature_1": [1.0, 2.0, 3.0],
-        "feature_2": [4.0, 5.0, 6.0],
-        "target": [2.5, 3.5, 4.5],
-    })
+    data = pd.DataFrame(
+        {
+            "date": [
+                "2021-11-23 00:00:00",
+                "2021-11-24 00:00:00",
+                "2021-11-25 00:00:00",
+            ],
+            "feature_1": [1.0, 2.0, 3.0],
+            "feature_2": [4.0, 5.0, 6.0],
+            "target": [2.5, 3.5, 4.5],
+        }
+    )
     data["date"] = pd.to_datetime(data["date"])
     return Dataset(pid=uuid.uuid4(), shape=data_shape, data=data)
 
 
 @pytest.fixture(scope="module")
 def ref_dataset(data_shape: DataShape) -> Dataset:
-    data = pd.DataFrame({
-        "feature_1": [1.0, 2.0, 3.0, 4.0, 5.0],
-        "feature_2": [5.0, 6.0, 7.0, 8.0, 9.0],
-        "target": [2.0, 3.0, 4.0, 5.0, 6.0],
-    })
+    data = pd.DataFrame(
+        {
+            "feature_1": [1.0, 2.0, 3.0, 4.0, 5.0],
+            "feature_2": [5.0, 6.0, 7.0, 8.0, 9.0],
+            "target": [2.0, 3.0, 4.0, 5.0, 6.0],
+        }
+    )
     # Training dataset does not have date column
     # data["col_timestamp"] = pd.to_datetime(data["col_timestamp"])
     return Dataset(
@@ -108,3 +120,4 @@ def test_mean_squared_error_metric(
 ) -> None:
     metrics = mean_squared_error_metric(data_shape, ref_model, test_dataset, y_pred)
     assert len(metrics) == 1
+    assert metrics[0].score == pytest.approx(0.02)
