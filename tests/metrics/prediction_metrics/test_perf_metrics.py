b46a702de1a7854be15084fdcb1bc9f358bbf640
John P. <john.pop@hotmail.com>
2025-11-25 16:51:22 +0100

    Added metrics

--- tests/metrics/prediction_metrics/test_perf_metrics.py
+++ tests/metrics/prediction_metrics/test_perf_metrics.py
@@ -4,6 +4,9 @@ import numpy as np
 from a4s_eval.data_model.evaluation import Dataset, DataShape, Model
 from a4s_eval.metrics.prediction_metrics.perf_metrics import (
     classification_accuracy_metric,
+    classification_error_rate_metric,
+    classification_balanced_accuracy_metric,
+    classification_confusion_matrix_metric,
     classification_f1_score_metric,
     classification_matthews_corrcoef_metric,
     classification_precision_metric,
@@ -23,7 +26,7 @@ def test_smoke(
     assert len(metrics) == 0
 
 
-def test_model_accuracy_evaluation(
+def test_classification_accuracy_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
@@ -38,7 +41,56 @@ def test_model_accuracy_evaluation(
     assert isinstance(metrics[0].time, datetime.datetime)
 
 
-def test_model_f1_score_evaluation(
+def test_classification_error_rate_metric(
+    data_shape: DataShape,
+    ref_model: Model,
+    test_dataset: Dataset,
+    y_pred_proba: np.ndarray,
+):
+    metrics = classification_error_rate_metric(
+        data_shape, ref_model, test_dataset, y_pred_proba
+    )
+    assert len(metrics) == 1
+    assert metrics[0].name == "Error Rate"
+    assert isinstance(metrics[0].score, float)
+    assert isinstance(metrics[0].time, datetime.datetime)
+
+
+def test_classification_balanced_accuracy_metric(
+    data_shape: DataShape,
+    ref_model: Model,
+    test_dataset: Dataset,
+    y_pred_proba: np.ndarray,
+):
+    metrics = classification_balanced_accuracy_metric(
+        data_shape, ref_model, test_dataset, y_pred_proba
+    )
+    assert len(metrics) == 1
+    assert metrics[0].name == "Balanced Accuracy"
+    assert isinstance(metrics[0].score, float)
+    assert isinstance(metrics[0].time, datetime.datetime)
+
+
+def test_classification_confusion_matrix_metric(
+    data_shape: DataShape,
+    ref_model: Model,
+    test_dataset: Dataset,
+    y_pred_proba: np.ndarray,
+):
+    metrics = classification_confusion_matrix_metric(
+        data_shape, ref_model, test_dataset, y_pred_proba
+    )
+    assert len(metrics) >= 4 # minimum 2*2 matrix
+
+    for metric in metrics:
+        print(metric)
+        assert metric.name == "Confusion Matrix"
+        assert metric.description != ""
+        assert isinstance(metric.score, float)
+        assert isinstance(metric.time, datetime.datetime)
+
+
+def test_f1_score_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
@@ -53,7 +105,7 @@ def test_model_f1_score_evaluation(
     assert isinstance(metrics[0].time, datetime.datetime)
 
 
-def test_model_precision_evaluation(
+def test_precision_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
@@ -68,7 +120,7 @@ def test_model_precision_evaluation(
     assert isinstance(metrics[0].time, datetime.datetime)
 
 
-def test_model_recall_evaluation(
+def test_recall_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
@@ -83,7 +135,7 @@ def test_model_recall_evaluation(
     assert isinstance(metrics[0].time, datetime.datetime)
 
 
-def test_model_matthews_corrcoef_evaluation(
+def test_matthews_corrcoef_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
@@ -98,7 +150,7 @@ def test_model_matthews_corrcoef_evaluation(
     assert isinstance(metrics[0].time, datetime.datetime)
 
 
-def test_model_roc_auc_evaluation(
+def test_roc_auc_metric(
     data_shape: DataShape,
     ref_model: Model,
     test_dataset: Dataset,
